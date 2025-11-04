bf34584e95f81931dbc8635a97c8dde5e7c766a4
baynecheke <baynecheke@gmail.com>
2025-11-04 13:14:23 -0500

    small fix

--- server.py
+++ server.py
@@ -106,4 +106,4 @@ def run_game_loop():
 # --- Start The Server ---
 if __name__ == '__main__':
     print("Starting Flask server on http://localhost:5000")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True)
