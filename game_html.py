0f093ef50a13244136cb10829fd4fd693002e6e1
baynecheke <baynecheke@gmail.com>
2025-11-06 17:00:44 -0500

    moved file

--- game_html.py
+++ game_html.py
@@ -7,6 +7,7 @@ HTML_CONTENT = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Western Simulator</title>
+    **<link rel="icon" href="/static/favicon.ico">**
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap');
